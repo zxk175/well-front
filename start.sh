bfa507be514dcac6d41218e6ac909d826d185452
zxk175 <zxk175@qq.com>
2019-07-29 09:17:29 +0800

    edit sh

--- start.sh
+++ start.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-#参考地址：http://www.jianshu.com/p/1903cd80223c
-#Mac权限修改：chmod 777 start.sh
-#Linux权限修改：chmod a+x start.sh
-
-#0、删除原有的日志文件
-rm -f ./nohup.log
-
-#1、从 git 上拉取最新的代码
 git pull
 
-#2、后台运行新的 jar 文件
-npm run build > nohup.log 2>&1 &
-
-#3、休息 3 秒
-sleep 3
-
-#4、打印最新的日志
-tail -f ./nohup.log
+npm run build
