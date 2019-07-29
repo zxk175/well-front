75ade858686ff2c93c6bc9e8329fb431b96dc664
zxk175 <zxk175@qq.com>
2019-07-29 09:41:56 +0800

    edit image

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -32,7 +32,7 @@
     .login {
         width: 100%;
         height: 100%;
-        background-image: url('../../assets/images/login-bg.jpg');
+        background-image: url('../../assets/images/login-bg.png');
         background-size: cover;
         background-position: center;
         position: relative;
