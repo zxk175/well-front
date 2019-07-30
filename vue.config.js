3314b2e357539b1bd6df2a00c3806d04dcc3aa95
zxk175 <zxk175@qq.com>
2019-07-30 10:49:16 +0800

    edit js

--- vue.config.js
+++ vue.config.js
@@ -61,7 +61,9 @@ module.exports = {
 
                 new OptimizeCssPlugin(),
 
-                new BundleAnalyzerPlugin()
+                new BundleAnalyzerPlugin({
+                    analyzerPort: 1234
+                })
             ]
         };
     },
